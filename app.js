769a52b3e4df078fc5d3d927cce8aa3718b293e8
preciousken <pkehinde27@gmail.com>
2023-07-31 22:59:21 +0100

    deployed

--- app.js
+++ app.js
@@ -79,11 +79,28 @@ app.get("/get", async (req, res) => {
       }
     }
 
-    console.log(arrayData);
+    // console.log(arrayData);
+    function isDesiredTimeFrame(timeFrame) {
+      return /^(1|3|24)\s+Hours\s+Ago$/i.test(timeFrame);
+    }
+
+    // Filter the array and return only objects with the desired timeFrame
+    const filteredData = arrayData.filter((obj) => {
+      if (obj.log_events && Array.isArray(obj.log_events)) {
+        for (const logEvent of obj.log_events) {
+          if (logEvent.timeFrame && isDesiredTimeFrame(logEvent.timeFrame)) {
+            return true;
+          }
+        }
+      }
+      return false;
+    });
+
+    console.log(filteredData);
 
     return res.status(200).json({
       status: true,
-      data: arrayData,
+      data: filteredData,
     });
   } catch (error) {
     console.log(error);
@@ -100,107 +117,3 @@ app.listen(5000, () => {
 app.use("*", (req, res) => {
   return res.send("404");
 });
-
-// const express = require("express");
-// const app = express();
-// const axios = require("axios");
-// const cors = require("cors");
-// const data = require("./data");
-
-// app.use(express.json());
-// app.use(cors());
-
-// app.get("/", (req, res) => {
-//   return res.status(200).json({
-//     status: true,
-//     message: "Api fully functional",
-//   });
-// });
-
-// app.get("/getbyhour/:hour", async (req, res) => {
-//   const hour = Number(req.params.hour);
-//   try {
-// const apiUrl = "https://appslk-second.onrender.com/newEndPoint";
-// const response = await axios.get(apiUrl);
-
-//     function parseISODate(dateString) {
-//       return new Date(dateString);
-//     }
-
-//     // Get the current time.
-//     const currentTime = new Date();
-
-//     // Function to filter data based on time frame
-//     function filterDataByTimeFrame(arrayData, timeFrameHours) {
-//       return arrayData.filter((obj) => {
-//         // Check if obj.log_events exists and is an array
-//         if (Array.isArray(obj.log_events)) {
-//           return obj.log_events.some((e) => {
-//             const blockSignedAtTime = parseISODate(e.block_signed_at);
-
-//             // Calculate the time difference (in milliseconds)
-//             const timeDifference = currentTime - blockSignedAtTime;
-
-//             // Convert the time frame to milliseconds
-//             const timeFrameMilliseconds = timeFrameHours * 60 * 60 * 1000;
-//             return timeDifference <= timeFrameMilliseconds;
-//           });
-//         }
-//         return false;
-//       });
-//     }
-
-//     // Filter data for different time frames
-//     const filteredData = filterDataByTimeFrame(response.data, hour);
-
-//     const totalSum = filteredData.reduce((acc, item) => {
-//       const logEvents = item.log_events;
-
-//       // Check if log_events exists and is an array
-//       if (Array.isArray(logEvents)) {
-//         const eventSum = logEvents.reduce((eventAcc, logEvent) => {
-//           const decoded = logEvent.decoded;
-
-//           // Check if decoded exists and has the params property
-//           if (decoded && Array.isArray(decoded.params)) {
-//             const valueParam = decoded.params.find(
-//               (param) => param.name === "value"
-//             );
-
-//             // Check if the valueParam exists and its value can be converted to a number
-//             if (valueParam && !isNaN(Number(valueParam.value))) {
-//               const value = Number(valueParam.value);
-//               return eventAcc + value;
-//             }
-//           }
-
-//           return eventAcc; // If valueParam was not found or was not a valid number, return the current accumulator
-//         }, 0);
-
-//         return acc + eventSum;
-//       }
-
-//       return acc; // If logEvents was not found or was not an array, return the current accumulator
-//     }, 0);
-
-//     return res.status(200).json({
-//       status: true,
-//       data: filteredData,
-//       totalReturns: filteredData.length,
-//       totalSumOfParamsValue: totalSum,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     return res.json({
-//       status: false,
-//       message: "something completely went wrong.",
-//     });
-//   }
-// });
-// app.listen(5000, () => {
-//   console.log("app listening on port 5000");
-// });
-
-// app.use("*", (req, res) => {
-//   return res.send("404");
-// });
